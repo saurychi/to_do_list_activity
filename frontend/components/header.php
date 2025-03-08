e01130f0dde1359402503410c267e6154818ce03
walter.caballero <21700254@usc.edu.ph>
2025-03-08 22:10:07 +0800

    changed footer and main sizes distancing

--- frontend/components/header.php
+++ frontend/components/header.php
@@ -12,6 +12,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Google Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=add" />
 </head>
 <body>
     <header>
@@ -19,7 +22,6 @@
             <?php if(isset($_SESSION["user"])){ ?>
                 <form method="POST">
                     <p><?php echo "Welcome, " . $_SESSION["user"]; ?></p>
-                    <br>
                     <button type="button" onclick="logout();">Logout</button>
                 </form>
             <?php } ?>
--- frontend/pages/index.php
+++ frontend/pages/index.php
@@ -9,16 +9,21 @@
 <style>
     <?php include("../css/index.css")?>
 </style>
-    <h2>Tasks</h2>
-    <table border=1>
-        <tr>
-            <td>task id</td>
-            <td>Name</td>
-            <td>Description</td>
-            <td>Status</td>
-            <td>Expiry Date</td>
-            <td>Created at</td>
-        </tr>
+
+<main>
+    <h1>Tasks</h1>
+    <div id="filter_container">
+        <div>
+            <label for="options">Status:</label>
+            <select id="options" name="options">
+                <option value="1">Pending</option>
+                <option value="2">Done</option>
+            </select>
+        </div>
+        <a href="add_task.php">Add Task<span class="material-symbols-outlined">add</span></a>
+    </div>
+
+    <div id="tasks_container">
         <?php
             include('../../backend/config.php');
             $rows  = mysqli_query($conn, "SELECT * FROM tasks");
@@ -26,29 +31,24 @@
         ?>
 
         <?php foreach($rows as $row) : ?>
-        <tr id= <?php echo $row["task_id"]; ?>>
-            <td><?php echo $i++; ?></td>
-            <td><?php echo $row["task_name"]; ?></td>
-            <td><?php echo $row["description"]; ?></td>
-            <td><?php echo $row["status"]; ?></td>
-            <td><?php echo $row["expiry_date"]; ?></td>
-            <td><?php echo $row["created_at"]; ?></td>
-            <td>
-                <a href="edit_task.php?id=<?php echo $row["task_id"]; ?>">Edit</a>
-            </td>
-            <td>
-                <input type="checkbox" class="status-checkbox" data-id="<?php echo $row["task_id"]; ?>"
-                <?php echo ($row["status"] === "done") ? "checked" : ""; ?>>
-            </td>
-        </tr>
+        <ul>
+            <li id= <?php echo $row["task_id"]; ?>>
+                <div>
+                    <h2><?php echo $row["task_name"]; ?></h2>
+                    <p><?php echo $row["description"]; ?></p>
+                    <p>Due: <?php echo $row["expiry_date"]; ?></p>
+                </div>
+                <div>
+                    <a href="edit_task.php?id=<?php echo $row["task_id"]; ?>">Edit</a>
+
+                    <input type="checkbox" class="status-checkbox" data-id="<?php echo $row["task_id"]; ?>"
+                    <?php echo ($row["status"] === "done") ? "checked" : ""; ?>>
+                </div>
+            </li>
+        </ul>
         <?php endforeach; ?>
-
-    </table>
-
-    <br><br>
-
-    <a href="add_task.php">Add Task</a>
-
+    </div>
+</main>
 <?php
     include('../components/footer.php');
 ?>
